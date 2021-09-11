dde6e9d6553f7a04ce293936914b3e61b471a91f
Suraj Kumar <surajtakwar0786@gmail.com>
2021-09-12 01:58:09 +0530

    Day 2 Python task

--- day2.py
+++ day2.py
@@ -13,4 +13,71 @@ def arg(*args):
     print(sum)
 arg(1,2,3,4,5,10)
 
-print("######## TASK 2 ##############")
+print()
+print("############ TASK 2 ############")
+# Task 2: WAP to create a function in which the arguements are users and their marks and return three value.
+# User with maximum mark, Average_marks, Users below failing marks. Below is sample of output:
+# ("Raul": 99),("avg_marks","55"),["Hidan", "Goku", "Timon", "Sasuke", "Saitama"]
+def my_result(**kwargs):
+    max_marks=()
+    failed =[]
+    max_marks=[(i, max(kwargs.values())) for i in kwargs if max(kwargs.values()) == kwargs[i]]
+    avg_marks=("avg_marks:", str(sum(kwargs.values())/len(kwargs)))
+    for i in kwargs:
+        if kwargs[i] < 60:
+            failed.append(i)
+    return f"{max_marks},{avg_marks},{failed}"
+
+print(my_result(raul=90,Hidan=50,Goku=30,Timon=50,Sasuke=60, Saitama=80))
+
+print()
+print("############ TASK 3 ############")
+#TASK 3: WAP to traverse the below given dictionary and print every information about the user.
+
+a_dict = [
+    {
+        "name": "gara",
+        "power": "some sand related jutsu",
+        "powerlevel": 199,
+        "frieds": [
+            {
+                "name": "Naruto",
+                "friend_points": 28,
+                "enemies": ["Saitama"] 
+            },
+            {
+                "name": "Boruto",
+                "friend_points": 18,
+                "enemies": ["Saitama"]
+            }
+        ]
+    },
+    {
+        "name": "Alex",
+        "power": "some titans powers",
+        "powerlevel": 1199,
+        "frieds": [
+            {
+            "name": "Soniya",
+            "friend_points": 128,
+            "enemies": ["Saitama"] 
+            }
+        ]
+    },
+    {
+        "name": "King",
+        "power": "some titans powers",
+        "powerlevel": 1199,
+        "frieds": [
+            {
+            "name": "Saitama",
+            "friend_points": 128,
+            "enemies": ["Naruto", "gara", "boruto"] 
+            }
+        ]
+    }
+    
+]
+
+for i in a_dict:
+    print(i, end="\n")
